737888f4434ea5e6617fa0cf5662a4cae9e219b9
liucong <372360395@qq.com>
2019-12-03 17:05:57 +0800

    添加getcwd

--- test_suite.py
+++ test_suite.py
@@ -4,33 +4,33 @@ import sys, os
 print(sys.path)
 sys.path.append(os.getcwd() + '/scripts/')
 print(sys.path)
-from scripts import TestDemo
+# from scripts import TestDemo
 from tool import HTMLTestRunner
 
 a = 3
-if a == 1:
-    """TestSuite添加测试用例"""
-    suite = unittest.TestSuite()
-    # 添加单个测试方法
-    suite.addTest(TestDemo('test_add1'))
-    # 添加多个测试方法
-    # suite.addTests([TestDemo('test_add'), TestDemo('test_logger')])
-    # 添加所有测试方法
-    # suite.addTest(unittest.makeSuite(TestDemo))
-    # 运行测试套件
-    unittest.TextTestRunner().run(suite)
-
-elif a == 2:
-    """TestLoader添加测试用例"""
-    # 方式1：
-    suite = unittest.TestLoader().discover("./", pattern='test_unittest.py')
-    # 运行搜到的测试用例组成的测试套件
-    unittest.TextTestRunner().run(suite)
-
-    # 方式2：
-    # suite = unittest.defaultTestLoader.discover("./", pattern='test_unittest.py')
-    # unittest.TextTestRunner().run(suite)
-elif a == 3:
+# if a == 1:
+#     """TestSuite添加测试用例"""
+#     suite = unittest.TestSuite()
+#     # 添加单个测试方法
+#     suite.addTest(TestDemo('test_add1'))
+#     # 添加多个测试方法
+#     # suite.addTests([TestDemo('test_add'), TestDemo('test_logger')])
+#     # 添加所有测试方法
+#     # suite.addTest(unittest.makeSuite(TestDemo))
+#     # 运行测试套件
+#     unittest.TextTestRunner().run(suite)
+#
+# elif a == 2:
+#     """TestLoader添加测试用例"""
+#     # 方式1：
+#     suite = unittest.TestLoader().discover("./", pattern='test_unittest.py')
+#     # 运行搜到的测试用例组成的测试套件
+#     unittest.TextTestRunner().run(suite)
+#
+#     # 方式2：
+#     # suite = unittest.defaultTestLoader.discover("./", pattern='test_unittest.py')
+#     # unittest.TextTestRunner().run(suite)
+if a == 3:
 
     """TestLoader添加测试用例并生成HTML测试报告"""
     suite = unittest.TestLoader().discover("./scripts/", pattern='test_ddt.py')
